5d0fff76b9d834bc418b4d9442c38564420b1471
Ben Krier <benkrier@cna-83-136.cna-17.depaul.edu>
2018-05-29 11:20:31 -0500

    change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import store from "./store";
 import routes from "./routes";
 
 const App = () => (
   <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <div className="App">{routes}</div>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
